f20c949cc8e600d9b0309014b0083d0d872f302b
Samir <samihneto@gmail.com>
2025-05-28 11:05:23 -0300

    Atualização da requisição

--- gs1-safespace/src/screens/app/SolicitarAjuda/index.tsx
+++ gs1-safespace/src/screens/app/SolicitarAjuda/index.tsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, FlatList, TextInput, Alert } from 'react-native';
+import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 type Ajuda = {
   id: string;
@@ -10,46 +11,59 @@ type Ajuda = {
 export default function SolicitarAjuda() {
   const [ajudas, setAjudas] = useState<Ajuda[]>([]);
   const [descricao, setDescricao] = useState('');
-  const [idUsuarioSS, setIdUsuarioSS] = useState('');
 
-  const API_URL = 'http://192.168.15.5:5000/SolicitacaoAjuda'; // Altere para seu IP e rota real
+  const API_URL = 'http://187.72.164.185:5279/SolicitacaoAjuda'; // Altere para seu IP e rota real
 
   const fetchAjudas = async () => {
     try {
       const res = await fetch(API_URL);
+
       const data = await res.json();
       setAjudas(data);
+
     } catch (error) {
+      console.log(error.response.data)
       if (error instanceof Error) {
-        Alert.alert('Erro ao buscar dados', error.message);
+        Alert.alert('Erro ao buscar dados 1', error.message);
       } else {
-        Alert.alert('Erro ao buscar dados', 'Ocorreu um erro desconhecido');
+        Alert.alert('Erro ao buscar dados 2', 'Ocorreu um erro desconhecido');
       }
     }
   };
 
   const criarAjuda = async () => {
     try {
+      /**
       const response = await fetch(API_URL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           descricao,
-          dataSolicitacao: new Date().toISOString(),
-          idUsuarioSS,
+          dataSolicitacao: new Date().toISOString()
         }),
       });
 
-      if (!response.ok) throw new Error('Erro ao criar ajuda');
+      if (!response.ok) {
+        const data = response.json()
+        console.log(JSON.stringify(data, null, 2))
+        throw new Error('Erro ao criar ajuda 1');
+      }
 
       await fetchAjudas();
       setDescricao('');
-      setIdUsuarioSS('');
+       */
+      await axios.post("http://localhost:5279/SolicitacaoAjuda", {
+        descricao,
+      })
+
     } catch (error) {
+      console.log(" --->")
+      console.log(error)
+      console.log(" --->")
       if (error instanceof Error) {
-        Alert.alert('Erro ao criar ajuda', error.message);
+        Alert.alert('Erro ao criar ajuda 2', error.message);
       } else {
-        Alert.alert('Erro ao criar ajuda', 'Ocorreu um erro desconhecido');
+        Alert.alert('Erro ao criar ajuda 3', 'Ocorreu um erro desconhecido');
       }
     }
   };
